aab81f3c17a0b362e3e24e8b51d0141902b07180
AyaanGH <25395404+AyaanGH@users.noreply.github.com>
2020-06-01 19:47:40 +0100

    Add project files.

---
+++ Chip8/cpu.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+class Cpu
+{
+
+
+	//Public data members
+	public:
+
+		uint8_t registers[16]; //Registers
+		uint8_t memory[0xFFF];
+		uint16_t indexRegister;
+		uint16_t programCounter; //Starts at address 0x200
+		uint16_t stack[16];
+		uint8_t stackPointer;
+		uint8_t delayTimer;
+		uint8_t soundTimer;
+		uint8_t keypad[16];
+		uint32_t video[64 * 32];
+		uint16_t opcode;
+
+	
+	private:
+		void loadROM();
+
+
+
+};
+
