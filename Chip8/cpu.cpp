fbfae562c1bb38dfdf428b101c38cddfe8b3b125
AyaanGH <25395404+AyaanGH@users.noreply.github.com>
2020-07-19 14:00:51 +0100

    Added all instructions

--- Chip8/cpu.cpp
+++ Chip8/cpu.cpp
@@ -403,3 +403,258 @@ void Cpu::OP_Cxkk()
 }
 
 
+//Dxyn - DRW Vx, Vy, nibble Display n - byte sprite starting at memory location I at(Vx, Vy), set VF = collision.
+
+
+
+void Cpu::OP_Dxyn()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t vy = (opcode & 0x00F0u) >> 4u;
+	uint8_t height = opcode & 0x000Fu;
+
+
+	int8_t xPos = registers[vx] % VIDEO_WIDTH;
+	uint8_t yPos = registers[vy] % VIDEO_HEIGHT;
+
+	registers[0xF] = 0;
+
+
+	for (unsigned int row = 0; row < height; ++row)
+	{
+
+		uint8_t spriteByte = memory[indexRegister + row];
+
+		for (unsigned int column = 0; column < 8; ++column)
+		{
+			uint8_t spritePixel = spriteByte & (0x80u >> column);
+			uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + column)];
+
+			if (spritePixel)
+			{
+				if (*screenPixel == 0xFFFFFFFF)
+				{
+					registers[0xF] = 1;
+				}
+
+				*screenPixel ^= 0xFFFFFFFF;
+			}
+
+		}
+	}
+
+}
+
+
+
+///Ex9E - 
+
+//SKP Vx, Skip next instruction if key with the value of Vx is pressed.
+
+
+
+
+void Cpu::OP_Ex9E()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+
+	uint8_t key = registers[vx];
+
+	if (keypad[key])
+	{
+		programCounter += 2;
+	}
+}
+
+
+//ExA1 - SKNP Vx 
+//Skip next instruction if key with the value of Vx is not pressed.
+
+
+void Cpu::OP_ExA1()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+
+	uint8_t key = registers[vx];
+
+	if (!keypad[key])
+	{
+		programCounter += 2;
+	}
+
+}
+
+//Fx07 - LD Vx, DT, Set Vx = delay timer value.
+
+void Cpu::OP_Fx07()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+
+	registers[vx] = delayTimer;
+}
+
+
+//Fx0A - LD Vx, K Wait for a key press, store the value of the key in Vx.
+
+void Cpu::OP_Fx0A()
+{
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+	if (keypad[0])
+	{
+		registers[Vx] = 0;
+	}
+	else if (keypad[1])
+	{
+		registers[Vx] = 1;
+	}
+	else if (keypad[2])
+	{
+		registers[Vx] = 2;
+	}
+	else if (keypad[3])
+	{
+		registers[Vx] = 3;
+	}
+	else if (keypad[4])
+	{
+		registers[Vx] = 4;
+	}
+	else if (keypad[5])
+	{
+		registers[Vx] = 5;
+	}
+	else if (keypad[6])
+	{
+		registers[Vx] = 6;
+	}
+	else if (keypad[7])
+	{
+		registers[Vx] = 7;
+	}
+	else if (keypad[8])
+	{
+		registers[Vx] = 8;
+	}
+	else if (keypad[9])
+	{
+		registers[Vx] = 9;
+	}
+	else if (keypad[10])
+	{
+		registers[Vx] = 10;
+	}
+	else if (keypad[11])
+	{
+		registers[Vx] = 11;
+	}
+	else if (keypad[12])
+	{
+		registers[Vx] = 12;
+	}
+	else if (keypad[13])
+	{
+		registers[Vx] = 13;
+	}
+	else if (keypad[14])
+	{
+		registers[Vx] = 14;
+	}
+	else if (keypad[15])
+	{
+		registers[Vx] = 15;
+	}
+	else
+	{
+		programCounter -= 2;
+	}
+}
+
+
+
+//Fx15 - LD DT, Vx Set delay timer = Vx.
+
+void Cpu::OP_Fx15()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+
+	delayTimer = registers[vx];
+}
+
+//Fx18 - LD ST, Vx, Set sound timer = Vx.
+
+void Cpu::OP_Fx18()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+
+	soundTimer = registers[vx];
+}
+
+//Fx1E - ADD I, Vx Set I = I + Vx.
+
+void Cpu::OP_Fx1E()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+
+	indexRegister += registers[vx];
+}
+
+
+//Set I = location of sprite for digit Vx.
+
+void Cpu::OP_Fx29()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t digit = registers[vx];
+
+	indexRegister = 0x50 + (5 * digit);
+}
+
+
+
+
+
+//Fx33 - LD B, Vx, Store BCD representation of Vx in memory locations I, I + 1, and I + 2.
+
+
+void Cpu::OP_Fx33()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t value = registers[vx];
+
+	// Ones-place
+	memory[indexRegister + 2] = value % 10;
+	value /= 10;
+
+	// Tens-place
+	memory[indexRegister + 1] = value % 10;
+	value /= 10;
+
+	// Hundreds-place
+	memory[indexRegister] = value % 10;
+}
+
+
+void Cpu::OP_Fx55()
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+
+	for (uint8_t i = 0; i <= vx; ++i)
+	{
+		memory[indexRegister + i] = registers[i];
+	}
+}
+
+
+
+void Cpu::OP_Fx65()
+
+{
+	uint8_t vx = (opcode & 0x0F00u) >> 8u;
+
+	for (uint8_t i = 0; i <= vx; ++i)
+	{
+		registers[i] = memory[indexRegister + i];
+	}
+
+}
